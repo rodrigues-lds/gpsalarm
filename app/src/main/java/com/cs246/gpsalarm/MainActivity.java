750360360ccedaff516f4c03c7fb1836f9fe49df
Eduardo Rodrigues <rodrigues_lds@byui.edu>
2020-03-07 18:25:34 -0300

    Corretion on layout

--- app/src/main/java/com/cs246/gpsalarm/MainActivity.java
+++ app/src/main/java/com/cs246/gpsalarm/MainActivity.java
@@ -1,7 +1,6 @@
 package com.cs246.gpsalarm;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
