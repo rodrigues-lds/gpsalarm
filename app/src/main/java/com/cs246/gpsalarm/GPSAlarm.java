a8300e9ed8edcf00771356c37b9d1538364f07c7
robbzzilla <60374129+robbzzilla@users.noreply.github.com>
2020-03-07 13:38:25 -0600

    Updated main and added Alarm class

--- app/src/main/java/com/cs246/gpsalarm/GPSAlarm.java
+++ app/src/main/java/com/cs246/gpsalarm/GPSAlarm.java
@@ -1,9 +1,39 @@
 package com.cs246.gpsalarm;
 
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
 /**
  * Created by Eduardo A Rodrigues on 2/24/2020.
  */
-public class GPSAlarm {
+public class GPSAlarm extends Service {
+
+    Alarm alarm = new Alarm();
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        alarm.setAlarm(this);
+        return START_STICKY;
+    }
+
+    public void onStop()
+    {
+        alarm.cancelAlarm(this);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
     /*
     This function converts the Radius to Kilometers.
